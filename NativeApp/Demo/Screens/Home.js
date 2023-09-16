b8f3fa8f47f7acae977a20c0508446fbf165f1d7
Shrey <201210041@nitdelhi.ac.in>
2023-09-17 02:11:21 +0530

    Basic login logout

--- NativeApp/Demo/Screens/Home.js
+++ NativeApp/Demo/Screens/Home.js
@@ -1,10 +1,66 @@
-import { Text,View,Button } from "react-native"
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar'; // Add Image to the import statement
+import { StyleSheet,Button, Text, View, TextInput, Dimensions, TouchableOpacity } from 'react-native';
 export default function Home({navigation})
 {
-    return (
-        <View>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+  const [isFormValid, setIsFormValid] = useState(false);
 
+
+  useEffect(() => {
+  
+    // Trigger form validation when name, 
+    // email, or password changes
+    validateForm();
+}, [ email, password]);
+
+const validateForm = () => {
+    let errors = {};
+
+
+    // Validate email field
+    if (!email) {
+        errors.email = 'Email is required.';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+        errors.email = 'Email is invalid.';
+    }
+
+    // Validate password field
+    if (!password) {
+        errors.password = 'Password is required.';
+    } else if (password.length < 6) {
+        errors.password = 'Password must be at least 6 characters.';
+    }
+
+    // Set the errors and update form validity
+    setErrors(errors);
+    setIsFormValid(Object.keys(errors).length === 0);
+};
+
+    return (
+        <View style={styles.container}>
+          <Text style={styles.title} >Welcome to MyApp</Text>
+
+          <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+
+      />
+       <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry
+      />
+
+<View style={styles.buttonContainer}>
             <Button title="Login"
+            // style={[styles.button, { backgroundColor: 'red' }]}
             onPress={()=>navigation.navigate("DashBoard")}
             />
             <Button title="Register"
@@ -13,7 +69,57 @@ export default function Home({navigation})
             <Button title="TestScreen"
             onPress={()=>navigation.navigate("TestScreen")}
             />
+
+            </View>
+
+            {Object.values(errors).map((error, index) => (
+                <Text key={index} style={styles.error}>
+                    {error}
+                </Text>
+            ))}
             
         </View>
     )
 }
+
+//styles
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    color: '#333',
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: '#fff',
+    width: width * 0.8,
+    padding: 15,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: width * 0.8,
+  },
+  button: {
+    width: '48%',
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  error: {
+    color: 'red',
+    fontSize: 10,
+    marginBottom: 12,
+},
+
+});
